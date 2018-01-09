92df60cfbd0a1dd5b8978807c1c2941a1fe572da
Trey <troyrijkaard@hotmail.co.uk>
2018-01-09 22:35:43 +0000

    feat(pmd): add files required to run pmd

--- app/src/main/java/com/chesire/chesiteandroidstaticanalysis/MainActivity.java
+++ app/src/main/java/com/chesire/chesiteandroidstaticanalysis/MainActivity.java
@@ -2,12 +2,22 @@ package com.chesire.chesiteandroidstaticanalysis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private String mCausePmdIssue = "Using this string inside the post below will cause an issue";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().post(new Runnable() {
+            @Override
+            public void run() {
+                Log.d("Tag", mCausePmdIssue);
+            }
+        });
     }
 }
